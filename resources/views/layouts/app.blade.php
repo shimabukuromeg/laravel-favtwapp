f9dbdeebe9d6d17d5192ee290f63c61cb0536a19
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-12-14 17:54:50 +0900

    [update] assetのヘルパーをhttpsに対応させるため修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,18 +6,19 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @if (App::environment('production'))
+      <script src="{{ secure_asset('js/app.js') }}" defer></script>
+      <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    @else
+      <script src="{{ asset('js/app.js') }}" defer></script>
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @endif
 </head>
 <body>
     <div id="app">
